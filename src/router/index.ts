77c821e24d1c7d21803585526b85fc23d79c786a
Rusu Iulian <iulianrusu29@gmail.com>
2026-05-04 18:02:46 +0300

    fix bug

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,7 +23,7 @@ const router = createRouter({
   routes,
 })
 
-router.beforeEach(async (to, from, next) => {
+router.beforeEach(async (to, _, next) => {
   const { data: { session } } = await supabase.auth.getSession()
   const isAuth = !!session
 
